72516e035a76c8f02ad572afe0d595d7db5945e8
Maximilian Küffner <maximilian.kueffner@tum.de>
2021-12-16 17:53:21 +0100

    stream of 100k samples tested -> works

--- measure.py
+++ measure.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-res = np.loadtxt('./measurement/cont_t-0GHz_2021-12-16_17-18-53.csv', skiprows=3, delimiter=',')
+res = np.loadtxt('./measurement/cont_t-0GHz_2021-12-16_17-49-58.csv', skiprows=0, delimiter=',')
 print(res.shape)
 
 plt.plot(res)
