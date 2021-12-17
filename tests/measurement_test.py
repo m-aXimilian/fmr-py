9bef3789b8ec85a339da091cea3c0fcd824194ff
Maximilian Küffner <maximilian.kueffner@tum.de>
2021-12-17 15:07:36 +0100

    refactor measurement_test.py to proper test

--- tests/measurement_test.py
+++ tests/measurement_test.py
@@ -1,3 +1,4 @@
+import unittest
 import os, sys
 import numpy as np
 import pyvisa as vi
@@ -10,36 +11,43 @@ sys.path.append(parentdir)
 
 import src.measurement as m
 
-def main() -> None:
-    logging.basicConfig(filename='./log/test.log', filemode='w', level=logging.DEBUG)
+class FMRComposedTest(unittest.TestCase):
 
-    rm = vi.ResourceManager()
-    
-    param = {
-        'rf-freq': 2,
-        'rf-p': 0,
-        'rf-rm': rm,    # vi resource manager
-        'rf-conf': './config/hp83508.yaml',
-        'H-set': np.linspace(0,100,10000)/100,
-        'N': 200000,
-        'rate': 1000,
-        'name': 'fmr-test',
-        'daq-dev': 'Dev1',
-        'ai': ['ai0', 'ai1'],
-        'ao': ['ao0'],
-        'impuls': 'ctr0',
-        'trigger': 'Ctr0InternalOutput',
-        'mode': TaskMode.TASK_COMMIT,
-        'read-edge': Edge.FALLING,
-        'write-edge': Edge.RISING,
-        'read-timeout': 30,
-        'buffer-size': 200
-    }
+    def output(self) -> None:
+        logging.basicConfig(filename='./log/test.log', filemode='w', level=logging.DEBUG)
 
-    meas = m.FMRMeasurement(param)
-    meas.cfg_measurement()
-    meas.start_measurement()
+        rm = vi.ResourceManager()
+        
+        param = {
+            'rf-freq': 2,
+            'rf-p': 0,
+            'rf-rm': rm,    # vi resource manager
+            'rf-conf': './config/hp83508.yaml',
+            'H-set': np.linspace(0,100,10000)/100,
+            'N': 2000,
+            'rate': 1000,
+            'name': 'fmr-test',
+            'daq-dev': 'Dev1',
+            'ai': ['ai0', 'ai1'],
+            'ao': ['ao0'],
+            'impuls': 'ctr0',
+            'trigger': 'Ctr0InternalOutput',
+            'mode': TaskMode.TASK_COMMIT,
+            'read-edge': Edge.FALLING,
+            'write-edge': Edge.RISING,
+            'read-timeout': 30,
+            'buffer-size': 200
+        }
 
+        self.meas = m.FMRMeasurement(param)
+        self.meas.cfg_measurement()
+        self.meas.start_measurement()
+
+        
+    def test_output(self):
+        self.output()
+        self.assertTrue(os.path.isfile(self.meas.f_name),
+            "Measurement file created.")        
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
