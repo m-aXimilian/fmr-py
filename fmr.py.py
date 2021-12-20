86dd33762b6cfb7be97a921f43fd4e65547bda2b
Maximilian Küffner <maximilian.kueffner@tum.de>
2021-12-20 12:46:42 +0100

    live plot in subplots

--- fmr.py.py
+++ fmr.py.py
@@ -37,6 +37,32 @@ def plotter(_fname, _params):
         plt.pause(plt_pause)
     plt.show()
 
+def plotter_subs(_fname, _params):
+    to_plot = os.path.isfile(_fname)
+    while not to_plot:
+        sleep(.2)
+        to_plot = os.path.isfile(_fname)
+    
+    reps = int(_params['N']/_params['buffer-size'])
+    plt_pause = _params['buffer-size']/_params['rate']
+
+    fig, (field, lock) = plt.subplots(2, 1)
+    
+    for i in range(reps):
+        field.cla()
+        lock.cla()
+        r = np.genfromtxt(_fname, delimiter=',', names=True, skip_header=3)
+        row_names = r.dtype.names
+        field.plot(r[row_names[0]],'g--', label=row_names[0])
+        field.plot(r[row_names[1]],'r', label=row_names[1])
+        lock.plot(r[row_names[2]], label=row_names[2])
+        lock.plot(r[row_names[3]], label=row_names[3])
+        field.legend(loc='upper left')
+        field.grid()
+        lock.legend(loc='upper left')
+        lock.grid()
+        plt.pause(plt_pause)
+    plt.show()
 
 def main():
     config = load_config('./config/init.yaml')
@@ -51,7 +77,7 @@ def main():
         'rf-rm': rm,    # vi resource manager
         'rf-conf': './config/hp83508.yaml',
         'h-max': 150,
-        'N': 20000,
+        'N': 50000,
         'rate': 1000,
         'name': 'fmr-test',
         'daq-dev': 'Dev1',
@@ -71,7 +97,7 @@ def main():
     tr.start()
     
     
-    plotter(name, params)
+    plotter_subs(name, params)
     
     
 def load_config(file_path):
--- measure.py
+++ measure.py
@@ -1,16 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fname = './measurement/fmr-test-2GHz_2021-12-20_11-47-07.csv'
+fname = './measurement/fmr-test-2GHz_2021-12-20_12-26-10.csv'
 
 #res = np.loadtxt('./measurement/cont_t-0GHz_2021-12-17_11-13-08.csv', skiprows=0, delimiter=',')
 res= np.genfromtxt(fname, delimiter=',',skip_header=3, names=True)
 print(res.shape)
-
+names = res.dtype.names
 print(res.dtype.names)
 
-for name in res.dtype.names:
-    plt.plot(res[name],label=name)
+# for name in res.dtype.names:
+#     plt.plot(res[name],label=name)
 
-plt.legend(loc='upper left')
+plt.plot(res[names[0]])
+#plt.legend(loc='upper left')
 plt.show()
