b500926a89a6b01822d6475c4ec102e8a70ca052
m-aXimilian <kueffnermax@gmail.com>
2021-12-21 14:39:51 +0100

    updated documentation in visa_devices

--- src/visa_devices.py
+++ src/visa_devices.py
@@ -10,10 +10,17 @@ import yaml
 
 
 class HP83508:
-    """Wrapper for HP83508 RF-source functions"""
+    """Wrapper for HP83508 RF-source functions. Open a resource from the _ID string and the resource manager 
+        _RM and save the resource object to ID.
+
+        Args:
+            _rm (pyvisa.ResourceManager): resource manager for the HP83508 visa device
+            _path (str): config-file for setting the name and the limits of the device.
+        
+        Raises:
+            yaml.YAMLError: if the config file cannot be read properly
+        """
     def __init__(self, _rm, _path) -> None:
-        """Open a resource from the _ID string and the resource manager 
-        _RM and save the resource object to ID."""
         
         with open(_path,'r') as f:
             try:
@@ -27,7 +34,11 @@ class HP83508:
     
 
     def setF(self, _f) -> None:
-        """Takes a desired set-frequency _F (GHz) and sends it to the device."""
+        """Takes a desired set-frequency and sends it to the device.
+
+        Args:
+            _f (float): set frequency in GHz
+        """
         if _f < self.boundaries['f-min'] or _f > self.boundaries['f-max']:
             logging.info('Set-frequency {}GHz outside boundaries'.format(_f))
             return
@@ -36,7 +47,11 @@ class HP83508:
 
 
     def setP(self, _p) -> None:
-        """Takes a desired set-power _P (dBm) and sends it to the device."""
+        """Takes a desired set-power and sends it to the device.
+
+        Args:
+            _p (float): set power in dBm
+        """
         if _p < self.boundaries['p-min'] or _p > self.boundaries['p-max']:
             logging.info('Set-power {}dBm outside boundaries'.format(_p))
             return
@@ -46,24 +61,42 @@ class HP83508:
 
 
 class NIUSB6259:
-    """Wrapper for the NI USB6259 DAQ card."""
+    """Wrapper for the NI USB6259 DAQ card.
+    """
     def __init__(self) -> None:
         self.task = daq.Task()
 
     
     def start(self) -> None:
+        """Start a task. 
+        """
         self.task.start()
 
 
     def __format_channel(self, _dev, _ch) -> str:
         """Compose a string for passing to the nidaqmx-API functions from 
-        the device ID _DEV and the channel _CH."""
+        the device ID and the channel name.
+
+        Args:
+            _dev (str): device name
+            _ch (str): channel name
+
+        Returns:
+            str: [description]
+        """
         return '{dev}/{ch}'.format(dev = _dev, ch = _ch)
     
     
     def __get_add_channel(self, _type='ai'):
         """Returns the appropriate function for adding a channel to 
-        the task based on the type _TYPE of the channel."""
+        the task based on the type of the channel.
+
+        Args:
+            _type (str, optional): type of the channel to add. Defaults to 'ai'.
+
+        Returns:
+            func: appropriate fuction for adding the channel
+        """
         if _type == 'ai':
             return self.task.ai_channels.add_ai_voltage_chan
         if _type == 'ao':
@@ -75,8 +108,13 @@ class NIUSB6259:
 
 
     def add_channels(self, _dev, _ch, _type='ai') -> None:
-        """_CH can be of type dict, list or string. Contents are added as 
-        voltage_channels of type _TYPE to the device _DEV."""
+        """Contents are added as voltage_channels of the specified type to the device.
+
+        Args:
+            _dev (str): device name
+            _ch (str, dict, list): channels can be string, dictionary or list!
+            _type (str, optional): channel type. Defaults to 'ai'.
+        """
 
         add_fun = self.__get_add_channel(_type)
                                
@@ -100,7 +138,15 @@ class NIUSB6259:
 
     def config_clk(self, _dev, _ch, _r, _t=1000, _m=TaskMode.TASK_COMMIT) -> None:
         """For a clock channel, set the sample rate _R (Hz) for device _DEV at channel _CH.
-        Sets the implicit timing _T (=sample number per channel) and task mode _M as well."""
+        Sets the implicit timing _T (=sample number per channel) and task mode _M as well.
+
+        Args:
+            _dev (str): device name
+            _ch (str): channel name
+            _r (float): sample rate (Hz)
+            _t (int, optional): sample number per channel. Defaults to 1000.
+            _m (nidaqmx.Task.TaskMode, optional): clock task mode. Defaults to TaskMode.TASK_COMMIT.
+        """
         if not isinstance(_ch, str): return
         
         self.task.co_channels.add_co_pulse_chan_freq(
@@ -117,15 +163,29 @@ class NIUSB6259:
 
     def config_sample_clk(self, _r, _trig, _edge, _s, _m=constants.AcquisitionType.FINITE) -> None:
         """Configure the the trigger and clock for IO-channels of task self.task.
-        With the sample rate _R, a trigger _TRIGG, the trigger edge _EDGE and a 
-        number of samples _S."""
+
+        Args:
+            _r (float): sample rate (Hz)
+            _trig (str): trigger source channel
+            _edge (nidaqmx.Edge): e.g. RISING or FALLING
+            _s (int): number of samples
+            _m (nidaqmx.constants.AquisitionType, optional): finite or continuous. Defaults to constants.AcquisitionType.FINITE.
+        """
         self.rate = _r
         self.task.timing.cfg_samp_clk_timing(_r, source=_trig, active_edge=_edge, samps_per_chan=_s, sample_mode=_m)
 
 
     def analog_read_n(self, _s, _t) -> np.array:
         """Read _S numbers of samples from the configured input channels with timeout _T and return
-        them in an array."""
+        them in an array.
+
+        Args:
+            _s (int): number of samples
+            _t (int): timeout (s)
+
+        Returns:
+            np.array: [description]
+        """
         if not self.task.channel_names:
             logging.debug('No channels added to read from. {}'.format(self.analog_read.__name__))
             return
@@ -136,7 +196,11 @@ class NIUSB6259:
 
 
     def analog_write(self, _arr) -> None:
-        """Write _ARR to the specified output channel."""
+        """Write the array to the specified output channel.
+
+        Args:
+            _arr (array): output array
+        """
         self.task.write(_arr)
 
 
